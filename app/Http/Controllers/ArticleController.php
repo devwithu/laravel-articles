d3999e47088c03de34762e9e884d254849735b3a
daejin <daejin@daejins-iMac.local>
2019-04-04 00:38:55 +0900

    some api , postman file

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -34,6 +34,16 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
         //
+        $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
+
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+
+        if($article->save()) {
+            return new ArticleResource($article);
+        }
+
     }
 
     /**
@@ -63,6 +73,12 @@ class ArticleController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Get article
+        $article = Article::findOrFail($id);
+
+        if($article->delete()) {
+            return new ArticleResource($article);
+        }
+
     }
 }
